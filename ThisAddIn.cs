f44e70c2781464b4025ed37291f960857c05b3e2
Simon Curtis <SimonCurtis@challengepackaging.co.uk>
2019-09-17 14:45:27 +0100

    Update

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -55,10 +55,6 @@ namespace Signature
      
         #region VSTO generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InternalStartup()
         {
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
